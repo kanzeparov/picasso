fc8a74185294aafa1db6d7d002fc4daa3012870a
Kanzeparov <kanzeparov@yandex.ru>
2020-02-15 20:16:27 +0300

    init project

---
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ImageView imageView = findViewById(R.id.image);
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);
+    }
+}
